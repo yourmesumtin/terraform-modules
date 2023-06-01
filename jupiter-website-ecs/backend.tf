ea768dbdc1aaf1ad22a0eaecdbd7a201176d82d0
ABAYOMI TAFA <91387295+yourmesumtin@users.noreply.github.com>
2023-06-01 10:35:58 -0500

    added workflow

--- jupiter-website-ecs/backend.tf
+++ jupiter-website-ecs/backend.tf
@@ -1,11 +1,20 @@
 # store the terraform state file in s3
+# terraform {
+#   backend "s3" {
+#     bucket         = "yourme-terraform-remote-state"
+#     key            = "jupiter-website-ecs.tfstate"
+#     region         = "us-east-1"
+#     profile        = "default"
+#     dynamodb_table = "terraform-state-lock"
+#     encrypt        = true
+#   }
+
 terraform {
-  backend "s3" {
-    bucket         = "yourme-terraform-remote-state"
-    key            = "jupiter-website-ecs.tfstate"
-    region         = "us-east-1"
-    profile        = "default"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
+  cloud {
+    organization = "Gihubactions"
+
+    workspaces {
+      name = "my-githubactions"
+    }
   }
 }
